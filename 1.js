1c8ef1d967a4c6377249550c6af78a14f3b6e576
PedroJosue123 <pedro.ponce@tecsup.edu.pe>
2023-09-25 23:50:24 -0500

    Laboratorio03

---
+++ 1.js
@@ -0,0 +1,8 @@
+var nombre = "Carlos"
+console.log(nombre);
+var numero = 1000;
+console.log (numero)
+var objeto  = { nombre: "Juan",
+apellido : "Perez",
+telefono : "1223"}
+console.log(objeto)
---
+++ 4.js
@@ -0,0 +1,8 @@
+var nombre = 'Juan'; 
+var apellido = 'Perez';
+var nombreCompleto = nombre + ' ' + apellido; 
+console.log(nombreCompleto);
+var nombreCompleto2 = 'Carlos' + ' ' + 'Lara';
+console.log(nombreCompleto2);
+var x = nombre + 2 + 4; 
+console.log(x);
